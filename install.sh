0adf4068b3aa488d5e8bf74b360548768caff90b
Carlos Estay <cestay@redhat.com>
2022-09-15 01:11:57 -0300

    removed unnecessary file

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Create project
-oc new-project health-integrations-demo 
+oc new-project health-integrations-demo
+oc project health-integrations-demo
 
 # Create secret to s3-to-kafka integration
 oc delete secret env-cfg-demo
