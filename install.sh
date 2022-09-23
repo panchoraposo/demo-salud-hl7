a66229dd27a8dc11e555abe923d6ca5adf12bfd4
Carlos Estay <cestay@redhat.com>
2022-09-23 18:37:37 -0300

    added kafka-to-nosql

--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-# Create project
-oc new-project health-integrations-demo
-oc project health-integrations-demo
+# aws cli - create s3
+aws s3api delete-bucket --bucket salud-hl7-demo --region us-east-1
+aws s3api create-bucket --bucket salud-hl7-demo --region us-east-1
 
-# Create secret to s3-to-kafka integration
+# Deploy mongodb
+oc delete deploy mongodb-hl7-demo
+oc delete svc mongodb-hl7-demo
+oc new-app mongo MONGO_INITDB_ROOT_USERNAME=demo MONGO_INITDB_ROOT_PASSWORD=demo --name=mongodb-hl7-demo -l app.openshift.io/runtime=mongodb
+
+# Create project
+oc new-project integrations-demo
+oc project integrations-demo
+
+# Create secret to demo
 oc delete secret env-cfg-demo
 oc create secret generic --from-env-file=./config/env.cfg env-cfg-demo
 
 # Deploy s3-to-kafka integration 
-oc apply -f s3-to-kafka/s3-to-kafka.yaml
+oc delete integration s3-to-kafka
+oc apply -f ./s3-to-kafka/s3-to-kafka.yaml
+
+# Deploy hl7-to-json
+# cd ./hl7-to-json/
+# quarkus build -Dquarkus.kubernetes.deploy=true
+# cd ..
+
+# Deploy kafka-to-nosql 
+oc delete integration kafka-to-nosql
+oc apply -f kafka-to-nosql/kafka-to-nosql.yaml
